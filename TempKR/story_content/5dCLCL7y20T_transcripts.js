352aebf61bec78953e410e40d4ff259479f424c9
Azorse <schisslerdg@gmail.com>
2024-11-20 13:22:14 -0700

    huntsman v3

--- TempKR/story_content/5dCLCL7y20T_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":256,"text":"Point Yout Toe and Go applies when when you are seated."},{"start":3392,"text":"Reaching without pointing your toe \r\ncan result in stressful twisting and bending."},{"start":8000,"text":"Take a few seconds to push your chair back,"},{"start":10592,"text":"then you can easily turn and point your toes in the right direction."},{"start":14272,"text":"This prevents unnecessary stress and strain."},{"start":17248,"text":"Your an push back and turn,"},{"start":18746,"text":"even when the chair doesn't pivot."}]}]};
-    window.globalLoadJsAsset('story_content/5dCLCL7y20T_transcripts.js', JSON.stringify(data));
-})();
--- TempKR/story_content/6odM85pQCDT_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[{"start":256,"text":"Allowing your back to bend forward into one continuous out-curve"},{"start":3801,"text":"increases strain and compression on your spine."},{"start":7456,"text":"To restore your spinal in-curve,"},{"start":9289,"text":"flex your knees,"},{"start":10752,"text":"raise your head to look forward,"},{"start":12896,"text":"and push your tailbone back."},{"start":14852,"text":"This should re-align your spine for maximum strength and less stress."}]}]};
-    window.globalLoadJsAsset('story_content/6odM85pQCDT_transcripts.js', JSON.stringify(data));
-})();
